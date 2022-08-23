f2eef416449bc6d5d9f0129caac833dcc99517d5
bendesousa <bdsousa069@gmail.com>
2022-08-23 09:50:33 +0100

    worked on drink class

--- PolymorphismLab/src/main/java/drinks/Drink.java
+++ PolymorphismLab/src/main/java/drinks/Drink.java
@@ -1,4 +1,33 @@
 package drinks;
 
-public class Drink {
+public abstract class Drink {
+
+    private String name;
+    private boolean alcoholic;
+    private String type;
+
+    public Drink (String name, boolean alcoholic, String type) {
+        this.name = name;
+        this.alcoholic = true;
+        this.type = type;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(){
+        this.name = name;
+    }
+
+    public String requireId(){
+        if(alcoholic = true);
+        return "Can I see your ID?";
+    }
+
+    public String offerStraw(){
+        if(type == "tall");
+        return "Would you like a straw?";
+    }
+
 }
