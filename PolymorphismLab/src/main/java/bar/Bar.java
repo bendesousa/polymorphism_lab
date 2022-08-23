9ccae4dcf320d16337dbc8b37df14b9b0afa5641
bendesousa <bdsousa069@gmail.com>
2022-08-23 18:54:29 +0100

    wrote bar class

--- PolymorphismLab/src/main/java/bar/Bar.java
+++ PolymorphismLab/src/main/java/bar/Bar.java
@@ -1,4 +1,35 @@
 package bar;
 
+import drinks.Drink;
+import stock.Stock;
+
 public class Bar {
+
+    private Stock alcoholicDrinks;
+    private Stock nonAlcoholicDrinks;
+
+    public Bar (Stock alcoholicDrinks, Stock nonAlcoholicDrinks){
+        this.alcoholicDrinks = alcoholicDrinks;
+        this.nonAlcoholicDrinks = nonAlcoholicDrinks;
+    }
+
+    public Stock getAlcoholicDrinks() {
+        return alcoholicDrinks;
+    }
+
+    public void setAlcoholicDrinks(Stock alcoholicDrinks) {
+        this.alcoholicDrinks = alcoholicDrinks;
+    }
+
+    public Stock getNonAlcoholicDrinks() {
+        return nonAlcoholicDrinks;
+    }
+
+    public void setNonAlcoholicDrinks(Stock nonAlcoholicDrinks) {
+        this.nonAlcoholicDrinks = nonAlcoholicDrinks;
+    }
+
+    public void addDrinkToStock (Drink drink, Stock stock){
+        stock.addDrinks(drink);
+    }
 }
--- PolymorphismLab/src/test/java/bar/BarTest.java
+++ PolymorphismLab/src/test/java/bar/BarTest.java
@@ -1,4 +1,33 @@
 package bar;
 
+import drinks.Drink;
+import drinks.Wine;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import stock.Stock;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BarTest {
+
+    private Bar bar;
+    private Stock cheapStock;
+    private Stock expensiveStock;
+
+    @BeforeEach
+    public void setUp(){
+        cheapStock = new Stock("Cheap Stock");
+        expensiveStock = new Stock("Expensive Stock");
+        bar = new Bar(cheapStock, expensiveStock);
+    }
+
+    @Test
+    public void canAddDrinkToStock(){
+        Drink drink = new Wine("House Wine", true, "short");
+        bar.addDrinkToStock(drink, expensiveStock);
+        int expected = 1;
+        int actual = expensiveStock.countDrinks();
+        assertEquals(expected, actual);
+    }
+
 }
