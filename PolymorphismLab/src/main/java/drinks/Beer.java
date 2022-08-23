fbfa9ca1997d363b96b296dc0c372b6bec62ca55
bendesousa <bdsousa069@gmail.com>
2022-08-23 14:57:20 +0100

    tested beer class

--- PolymorphismLab/src/main/java/drinks/Beer.java
+++ PolymorphismLab/src/main/java/drinks/Beer.java
@@ -4,13 +4,24 @@ public class Beer extends Drink {
 
     private boolean isFlat;
 
-    public Beer(String name, boolean alcoholic, String type, boolean isFlat){
+    public Beer(String name, boolean alcoholic, String type){
         super (name, alcoholic, type);
+        this.isFlat = isFlat();
+    }
+
+    public boolean isFlat(){
+        return this.isFlat;
+    }
+
+    public void setFlat(boolean isFlat){
         this.isFlat = isFlat;
     }
 
-    public String offerReplacement(){
-        if (isFlat = true);
-        return "Would you like a replacement?";
+    public String offerReplacement() {
+        if (isFlat = true) {
+            return "Let me get you another.";
+        } else {
+            return "Enjoy your drink!";
+        }
     }
 }
--- PolymorphismLab/src/test/java/drinks/BeerTest.java
+++ PolymorphismLab/src/test/java/drinks/BeerTest.java
@@ -1,4 +1,38 @@
 package drinks;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 public class BeerTest {
+
+    private Beer beer;
+
+    @BeforeEach
+    public void setUp(){
+        beer = new Beer("Fosters", true, "tall");
+    }
+
+    @Test
+    public void canRequestId(){
+        String expected = "Can I see your ID?";
+        String actual = beer.requireId();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void canOfferStraw(){
+        String expected = "Would you like a straw?";
+        String actual = beer.offerStraw();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void canOfferReplacement(){
+        String expected = "Let me get you another.";
+        String actual = beer.offerReplacement();
+        assertEquals(expected, actual);
+    }
 }
