fec106f4410f840b89e69877d377c7d754c193e7
bendesousa <bdsousa069@gmail.com>
2022-08-23 18:42:50 +0100

    wrote and tested stock class

--- PolymorphismLab/src/main/java/stock/Stock.java
+++ PolymorphismLab/src/main/java/stock/Stock.java
@@ -1,4 +1,40 @@
 package stock;
 
+import drinks.Drink;
+
+import java.util.ArrayList;
+
 public class Stock {
+
+    private String name;
+    private ArrayList<Drink> drinks;
+
+    public Stock(String name){
+        this.name = name;
+        this.drinks = new ArrayList<>();
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public ArrayList<Drink> getDrinks(){
+        return drinks;
+    }
+
+    public void setDrinks(ArrayList<Drink> drinks){
+        this.drinks = drinks;
+    }
+
+    public int countDrinks(){
+        return this.drinks.size();
+    }
+
+    public void addDrinks(Drink drink){
+        this.drinks.add(drink);
+    }
 }
--- PolymorphismLab/src/test/java/stock/StockTest.java
+++ PolymorphismLab/src/test/java/stock/StockTest.java
@@ -1,4 +1,46 @@
 package stock;
 
+import drinks.Beer;
+import drinks.Drink;
+import drinks.Wine;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StockTest {
+
+    private Stock stock;
+
+    @BeforeEach
+    public void setUp(){
+        stock = new Stock ("imaginary stock");
+    }
+
+    @Test
+    public void canCountDrinks(){
+        int expected = 0;
+        int actual = stock.countDrinks();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void canAddDrink(){
+        Drink drink = new Beer("Fosters", true, "tall");
+        stock.addDrinks(drink);
+        int expected = 1;
+        int actual = stock.countDrinks();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void canAddMultipleDrinks() {
+        Drink beer = new Beer("Fosters", true, "tall");
+        Drink wine = new Wine("House Wine", true, "short");
+        stock.addDrinks(beer);
+        stock.addDrinks(wine);
+        int expected = 2;
+        int actual = stock.countDrinks();
+        assertEquals(expected, actual);
+    }
 }
